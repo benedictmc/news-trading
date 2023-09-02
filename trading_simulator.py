35224619892278718b12faf2bf4a96efb87f5b11
Ben McGovern <benmcgovern13@gmail.com>
2023-09-02 16:25:34 +0000

    Adding

--- trading_simulator.py
+++ trading_simulator.py
@@ -29,13 +29,13 @@ class TradingSimulator():
         # self.expiry_dict = {}
         self.aggregation_window = '1S'
         self.data_type = "aggTrades"
+        self.symbol = symbol
+        self.date = date
         self.interval = "monthly"
         self.load_source = load_source
+        self.reduced_trades_filepath = f"reduced_trades/{self.interval}/{symbol}/{symbol}-reduced-{self.aggregation_window}-{self.data_type}-{date}.csv"
+        self.agg_trades_filepath = f"aggTrades/{self.interval}/{symbol}/{symbol}-aggTrades-{date}.csv"
         
-
-
-
-
 
     # -------------
     # News methods (Not used yet)
@@ -151,76 +151,70 @@ class TradingSimulator():
     # News methods End (Not used yet)
     # -------------
 
+    def build_reduced_trades(self):
 
-    def get_load_trades(self, blob_file_path):
-        try:
-            if self.load_source == "local":
-                trades_df = pd.read_csv("data/trades/BTCUSDT-aggTrades-2023-08.csv", index_col=0)
+        self.reduced_trades_df = self.get_reduced_trades()   
 
-            if self.load_source == "blob":
-                
-                retrieve_file = BLOB_SERVICE_CLIENT.get_blob_client(container=CONTAINER_NAME, blob=blob_file_path)    
-                
-                # Download the blob content
-                blob_data = retrieve_file.download_blob()
-                data = blob_data.readall()
-
-                # Convert bytes to string and then load into DataFrame
-                trades_df = pd.read_csv(io.BytesIO(data), index_col=0)
-            
-            return trades_df
+        if self.reduced_trades_df is None:
+            print("Could not build reduced trades")
+            return
         
-        except Exception as e:
-            print(f"Could not load trade from {self.load_source}")
-            print(f"Error: {e}")
-            return None
-    
-
-    def save_reduced_trades(self, reduced_trades_df, symbol, date):
+        
+    def save_reduced_trades(self, reduced_trades_df):
         csv_data = io.StringIO()
         reduced_trades_df.to_csv(csv_data, index=False)  # Set index to True if you want to include the DataFrame's index in the CSV
         csv_data.seek(0)
         
-        blob_file_path = f"reduced_trades/{self.interval}/{symbol}/{symbol}-reduced-{self.aggregation_window}-{self.data_type}-{date}.csv"
-        blob_client = BLOB_SERVICE_CLIENT.get_blob_client(container=CONTAINER_NAME, blob=blob_file_path)
+        blob_client = BLOB_SERVICE_CLIENT.get_blob_client(container=CONTAINER_NAME, blob=self.reduced_trades_filepath)
         blob_client.upload_blob(csv_data.getvalue(), overwrite=True)  # Set overwrite to True if you want to replace an existing blob
 
-    
-    def does_reduced_trades_exist(self, blob_file_path):
 
-        blob_file_service = BlobClient(account_url=ACCOUNT_URL, container_name="binancedata", blob_name=blob_file_path, credential=ACCOUNT_KEY)
+    def get_reduced_trades(self):
+        
+        reduced_trades_df = self.__retrieve_from_blob(self.reduced_trades_filepath)
 
-        # Try to get blob properties
-        try:
-            blob_file_service.get_blob_properties()
-            return True
-        except ResourceNotFoundError:
-            return False
+        if reduced_trades_df is None:
 
+            agg_trades_df = self.__retrieve_from_blob(self.agg_trades_filepath)
+            reduced_trades_df = self.__reduce_trades(agg_trades_df)
 
-    def reduce_trades(self, trades_df):
+            if reduced_trades_df is None:
+                return None
+            
+            self.save_reduced_trades(reduced_trades_df)
 
-        if trades_df is None:
+            reduced_trades_df.index = reduced_trades_df['flooored_time']
+            reduced_trades_df = reduced_trades_df.drop(columns=['flooored_time'])
+
+        else:
+            print(f"Retrieved reduced trades from blob for {self.symbol}-{self.date}")
+
+        return reduced_trades_df
+        
+        
+    def __reduce_trades(self, agg_trades_df):
+
+        if agg_trades_df is None:
             print("No trades to reduce")
             return None
 
         # Check if 'transact_time' is not already a datetime object
-        if not isinstance(trades_df['transact_time'].iloc[0], pd.Timestamp):
+        if not isinstance(agg_trades_df['transact_time'].iloc[0], pd.Timestamp):
             # Convert transact_time to datetime
-            trades_df['transact_time'] = pd.to_datetime(trades_df['transact_time'], unit='ms')
+            agg_trades_df['transact_time'] = pd.to_datetime(agg_trades_df['transact_time'], unit='ms')
 
         # Round transact_time to the nearest second
-        trades_df['flooored_time'] = trades_df['transact_time'].dt.floor(self.aggregation_window)
+        agg_trades_df['flooored_time'] = agg_trades_df['transact_time'].dt.floor(self.aggregation_window)
 
         # Calculate the number of trades for each row
-        trades_df['num_trades'] = trades_df['last_trade_id'] - trades_df['first_trade_id'] + 1
+        agg_trades_df['num_trades'] = agg_trades_df['last_trade_id'] - agg_trades_df['first_trade_id'] + 1
 
         # Compute average price for all transactions
-        overall_avg_price = trades_df.groupby('flooored_time')['price'].mean()
+        overall_avg_price = agg_trades_df.groupby('flooored_time')['price'].mean()
 
         # Split dataframe for buys and sells
-        buys = trades_df[trades_df['is_buyer_maker']]
-        sells = trades_df[~trades_df['is_buyer_maker']]
+        buys = agg_trades_df[agg_trades_df['is_buyer_maker']]
+        sells = agg_trades_df[~agg_trades_df['is_buyer_maker']]
 
         # Reduce separately for buy and sell
         buy_agg = buys.groupby('flooored_time').agg(
@@ -245,65 +239,57 @@ class TradingSimulator():
         # Rename the price column to avg_price
         agg_df.rename(columns={'price': 'avg_price'}, inplace=True)
 
-        agg_df.index = agg_df['flooored_time']
-        agg_df = agg_df.drop(columns=['flooored_time'])
-
+        # agg_df.index = agg_df['flooored_time']
+        # agg_df = agg_df.drop(columns=['flooored_time'])
         return agg_df
 
 
-    def plot_reduced_trades(self, agg_df):
-        fig, ax1 = plt.subplots(figsize=(15, 8))
+    # def plot_reduced_trades(self, reduced_trades_df):
+    #     fig, ax1 = plt.subplots(figsize=(15, 8))
 
-        # Line plot for average price
-        ax1.plot(agg_df['rounded_time'], agg_df['avg_price'], color='b', label='Average Price', linewidth=2)
-        ax1.set_xlabel('Time')
-        ax1.set_ylabel('Average Price', color='b')
-        ax1.tick_params(axis='y', labelcolor='b')
+    #     # Line plot for average price
+    #     ax1.plot(agg_df['rounded_time'], agg_df['avg_price'], color='b', label='Average Price', linewidth=2)
+    #     ax1.set_xlabel('Time')
+    #     ax1.set_ylabel('Average Price', color='b')
+    #     ax1.tick_params(axis='y', labelcolor='b')
 
-        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-        width = (agg_df['rounded_time'].iloc[1] - agg_df['rounded_time'].iloc[0]).seconds / (3 * 86400)  # Width of bars based on the time difference between entries
+    #     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    #     width = (agg_df['rounded_time'].iloc[1] - agg_df['rounded_time'].iloc[0]).seconds / (3 * 86400)  # Width of bars based on the time difference between entries
 
-        # Bar plots for volumes
-        ax2.bar(agg_df['rounded_time'], agg_df['sum_asset_bought'], width=width, alpha=0.6, label='Bought Volume', color='g')
-        ax2.bar(agg_df['rounded_time'], -agg_df['sum_asset_sold'], width=width, alpha=0.6, label='Sold Volume', color='r')
-        ax2.set_ylabel('Volume', color='g')  # Green for bought volume
-        ax2.tick_params(axis='y', labelcolor='g')
+    #     # Bar plots for volumes
+    #     ax2.bar(agg_df['rounded_time'], agg_df['sum_asset_bought'], width=width, alpha=0.6, label='Bought Volume', color='g')
+    #     ax2.bar(agg_df['rounded_time'], -agg_df['sum_asset_sold'], width=width, alpha=0.6, label='Sold Volume', color='r')
+    #     ax2.set_ylabel('Volume', color='g')  # Green for bought volume
+    #     ax2.tick_params(axis='y', labelcolor='g')
 
-        # Title and show the plot
-        plt.title('Trade Dynamics Over Time')
-        fig.tight_layout()  # Otherwise the right y-label is slightly clipped
-        plt.legend(loc='upper left')
-        plt.show()
+    #     # Title and show the plot
+    #     plt.title('Trade Dynamics Over Time')
+    #     fig.tight_layout()  # Otherwise the right y-label is slightly clipped
+    #     plt.legend(loc='upper left')
+    #     plt.show()
 
-
+    
+    def __retrieve_from_blob(self, blob_file_path):
+        try:
+            retrieve_file = BLOB_SERVICE_CLIENT.get_blob_client(container=CONTAINER_NAME, blob=blob_file_path)    
+                    
+            # Download the blob content
+            blob_data = retrieve_file.download_blob()
+            data = blob_data.readall()
+            df = pd.read_csv(io.BytesIO(data), index_col=0)
+            return df
+        
+        except ResourceNotFoundError:
+            return None
 
 SYMBOLS = [ 'LRCUSDT','BTCUSDT','ZECUSDT','EOSUSDT','SOLUSDT','XEMUSDT','OPUSDT','SNXUSDT','1INCHUSDT','TRXUSDT','QTUMUSDT','AGIXUSDT','RUNEUSDT','FLOWUSDT','BNBUSDT','HFTUSDT','APTUSDT','ANKRUSDT','DOGEUSDT','ASTRUSDT','RDNTUSDT','STXUSDT','CTKUSDT','ETHUSDT','NEARUSDT','TUSDT','IOTXUSDT','GRTUSDT','UNIUSDT','ZRXUSDT','DYDXUSDT','ICPUSDT','NEOUSDT','BNXUSDT','SANDUSDT','EGLDUSDT','SSVUSDT','GTCUSDT','MASKUSDT','AMBUSDT','DARUSDT','CELOUSDT','AAVEUSDT','HBARUSDT','ARBUSDT','SXPUSDT','ANTUSDT','ZENUSDT','ICXUSDT','XTZUSDT','YFIUSDT','RSRUSDT','PEOPLEUSDT','DGBUSDT','LINKUSDT','GALUSDT','FTMUSDT','FXSUSDT','TLMUSDT','CELRUSDT','SUSHIUSDT','ALPHAUSDT','ARPAUSDT','HOOKUSDT','MINAUSDT','COTIUSDT','JOEUSDT','ENSUSDT','WOOUSDT','INJUSDT','SKLUSDT','USDCUSDT','IMXUSDT','SFPUSDT','DASHUSDT','MAGICUSDT','PERPUSDT','CTSIUSDT','CHZUSDT','QNTUSDT','LEVERUSDT','IOTAUSDT','IOSTUSDT','WAVESUSDT','TOMOUSDT','BLZUSDT','C98USDT','VETUSDT','ZILUSDT','GMTUSDT','DOTUSDT','ROSEUSDT','LDOUSDT','XLMUSDT','CFXUSDT','LITUSDT','XVSUSDT','OCEANUSDT','BANDUSDT','HOTUSDT','LTCUSDT','AVAXUSDT','ENJUSDT','GALAUSDT','BATUSDT','FETUSDT','BALUSDT','FILUSDT','KAVAUSDT','RNDRUSDT','LPTUSDT','AUDIOUSDT','ALGOUSDT','XRPUSDT','OGNUSDT','GMXUSDT','ACHUSDT','ONTUSDT','KLAYUSDT','REEFUSDT','AXSUSDT','HIGHUSDT','LINAUSDT','ALICEUSDT','DUSKUSDT','FLMUSDT','PHBUSDT','ATOMUSDT','MATICUSDT','LQTYUSDT','STORJUSDT','CKBUSDT','KNCUSDT','MKRUSDT','APEUSDT','API3USDT','NKNUSDT','RVNUSDT','CHRUSDT','MANAUSDT','CRVUSDT','STMXUSDT','ADAUSDT','ATAUSDT','STGUSDT','ARUSDT','IDUSDT','RLCUSDT','THETAUSDT','BLURUSDT','ONEUSDT','TRUUSDT','TRBUSDT','COMPUSDT','IDEXUSDT','SUIUSDT','EDUUSDT','MTLUSDT','1000PEPEUSDT','1000FLOKIUSDT','DENTUSDT','BCHUSDT','1000XECUSDT','JASMYUSDT','UMAUSDT','BELUSDT','1000SHIBUSDT','RADUSDT','XMRUSDT','1000LUNCUSDT','SPELLUSDT','KEYUSDT','COMBOUSDT','UNFIUSDT','CVXUSDT','ETCUSDT','MAVUSDT','MDTUSDT','XVGUSDT','NMRUSDT','BAKEUSDT','WLDUSDT','PENDLEUSDT','ARKMUSDT','AGLDUSDT','YGGUSDT','SEIUSDT' ]
 date = "2023-07"
 
+
 for symbol in SYMBOLS:
-    print(f"Starting trade simulator for {symbol}-{date}")
     trading_simulator = TradingSimulator(symbol, date, load_source="blob")
-    
-    agg_blob_file_path = f"aggTrades/{trading_simulator.interval}/{symbol}/{symbol}-aggTrades-{date}.csv"
-    reduced_blob_file_path = f"reduced_trades/{trading_simulator.interval}/{symbol}/{symbol}-reduced-{trading_simulator.aggregation_window}-{trading_simulator.data_type}-{date}.csv"
-    
-    reduced_exists = trading_simulator.does_reduced_trades_exist(reduced_blob_file_path)
-
-    if reduced_exists:
-        print(f"Reduced trades already exist for {symbol}-{date}")
-        continue
-
-    trades_df = trading_simulator.get_load_trades(agg_blob_file_path)
-    
-    if trades_df is None:
-        print("No trades to reduce")
-        continue
-
-    reduced_trades_df = trading_simulator.reduce_trades(trades_df)
-    
-    
-
-    trading_simulator.save_reduced_trades(reduced_trades_df, symbol, date)
-
-# agg_trades_df.to_csv(f"data/aggregate/BTCUSDT-reduced-{agg_trades_df.aggregation_window}-aggTrades-2023-08.csv")
+    trading_simulator.build_reduced_trades()
+    print(f"Symbol: {symbol} - Date: {date}")
+    print("RESULT:")
+    print(trading_simulator.reduced_trades_df.head())
 
